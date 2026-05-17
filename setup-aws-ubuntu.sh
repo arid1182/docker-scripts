30a2c9b5ce007a9cc710aa956555bd81bd95be1a
Mubasher Hassan <81035765+arid1182@users.noreply.github.com>
2026-05-17 12:30:48 +0500

    Add HashiCorp Terraform installation to setup script
    Added installation steps for HashiCorp Terraform to the setup script, including GPG key setup and repository addition.

--- setup-aws-ubuntu.sh
+++ setup-aws-ubuntu.sh
@@ -38,10 +38,21 @@ unzip -u aws-sam-cli.zip -d sam-installation
 sudo ./sam-installation/install --update
 rm -rf sam-installation aws-sam-cli.zip
 
+# 5. AWS HashiCorp Terraform Installion/Upgrade
+echo "⚡ Processing AWS Terraformharshicorp ..."
+sudo apt-get install -y gnupg software-properties-common curl unzip
+echo "Installing Terraform..."
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update && sudo apt-get install -y terraform
+echo "Verifying installations..."
+echo "Installation complete! To configure your AWS credentials, run: 'aws configure'"
+
 echo "------------------------------------------------"
 echo "✅ Setup Complete! Versions installed:"
 aws --version
 sam --version
 docker --version
+terraform -version
 echo "------------------------------------------------"
 echo "💡 TIP: Please log out and back in to use Docker without sudo."
